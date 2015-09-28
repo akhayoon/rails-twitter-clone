fcf5a3defdd8c628053dcd95453a57126212382e
Hussien Khayoon <akhayoon@gmail.com>
2015-09-28 17:29:15 +0000

    solves ex 7.3

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,15 +5,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid sign up information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: {name: "",
-                              email: "user@invalid",
+      post users_path, user: {name: "test",
+                              email: "user@works.com",
                               password: "foo",
-                              password_confirmation: "bar" }
+                              password_confirmation: "foo" }
     end
     assert_template 'users/new'
   end
   
-  test "valid singup information" do
+  test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: {name: "Example User",
@@ -22,6 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                            password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
   
 end
